04f269cb061cfaf0971dfb518fbd5574b57934b6
aryanmajhi75 <majhiaryan13@gmail.com>
2024-08-10 17:04:25 +0530

    add chatbot

--- resqaid/lib/chatbot/chatbot.dart
+++ resqaid/lib/chatbot/chatbot.dart
@@ -1,12 +1,96 @@
-import 'package:flutter/material.dart';
-import 'package:resqaid/theme/theme_ext.dart';
+import 'dart:developer';
 
-class Chatbot extends StatelessWidget {
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+
+class Chatbot extends StatefulWidget {
   const Chatbot({super.key});
 
   @override
+  State<Chatbot> createState() => _ChatbotState();
+}
+
+class _ChatbotState extends State<Chatbot> {
+
+  // @override
+  // void initState(){
+  //   super.initState();
+  // }
+
+  //Defining the chat parameters like user, and list of chat messages
+  ChatUser user = ChatUser(
+    id: "0",
+    firstName: "User",
+  );
+
+  ChatUser gemini = ChatUser(
+    id: "gemini-chatbot-101",
+    firstName: "Gemini",
+  );
+
+  List<ChatMessage> messages = [];
+
+  //For storing the refernece of Gemini
+  final Gemini chatbot = Gemini.instance;
+
+  @override
   Widget build(BuildContext context) {
-    final appColors = context.appColors;
-    return FloatingActionButton(onPressed: (){},backgroundColor: appColors.accent,);
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Chat with Gemini",
+            style: TextStyle(
+                // color: Colors.amber,
+                ),
+          ),
+        ),
+        body: _chatpage());
+  }
+
+  Widget _chatpage() {
+    return DashChat(
+      messageOptions: MessageOptions(),
+      messages: messages,
+      currentUser: user,
+      onSend: _sendChat,
+    );
+  }
+
+  void _sendChat(ChatMessage chatMessages) {
+    setState(() {
+      messages = [chatMessages, ...messages];
+    });
+    try {
+      String userPrompt = chatMessages.text;
+      chatbot.streamGenerateContent(userPrompt).listen((event) {
+        ChatMessage? lastMessage = messages.firstOrNull;
+        if (lastMessage != null && lastMessage.user == gemini) {
+          setState(() {
+            lastMessage = messages.removeAt(0);
+            String response = event.content?.parts?.fold(
+                    "", (previous, current) => "$previous ${current.text}") ??
+                "";
+            // log(response);
+            lastMessage!.text += response;
+            messages = [lastMessage!, ...messages];
+          });
+        } else {
+          String response = event.content?.parts?.fold(
+                  "", (previous, current) => "$previous ${current.text}") ??
+              "";
+          ChatMessage message = ChatMessage(
+            user: gemini,
+            createdAt: DateTime.now(),
+            text: response,
+          );
+          setState(() {
+            messages = [message, ...messages];
+          });
+        }
+      });
+    } catch (e) {
+      log(e.toString());
+    }
   }
 }
