f9055d65924f624518dd4381ef7ff9665ea19289
aryanmajhi75 <majhiaryan13@gmail.com>
2024-08-10 14:27:19 +0530

    Update home.dart

--- resqaid/lib/home/home.dart
+++ resqaid/lib/home/home.dart
@@ -7,7 +7,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      floatingActionButton: Chatbot(),
+      // floatingActionButton: Chatbot(),
     );
   }
 }
