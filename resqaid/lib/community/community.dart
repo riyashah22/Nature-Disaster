49086716b6c1ac737a798227b2fd7fb5eb2c428b
Vansh Shah <shah.vansh132@gmail.com>
2024-08-10 14:58:28 +0530

    support screen: sos button created

--- resqaid/lib/community/community.dart
+++ resqaid/lib/community/community.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:resqaid/chatbot/chatbot.dart';
+import 'package:resqaid/theme/theme_ext.dart';
 
 class Community extends StatelessWidget {
   const Community({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final appColors = context.appColors;
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: appColors.error,
         onPressed: () {},
-        child: Icon(
-          Icons.emergency,
-        ),
+        tooltip: "SOS - Emergency",
+        label: const Text("SOS"),
+        extendedTextStyle: Theme.of(context).textTheme.titleMedium,
+        icon: const Icon(Icons.emergency_share_rounded),
       ),
       appBar: AppBar(
-        // backgroundColor: Colors.red,
-        title: Text("Support"),
+        title: const Text("Support"),
       ),
-      body: Center(
+      body: const Center(
         child: Text("Support Screen"),
       ),
     );
