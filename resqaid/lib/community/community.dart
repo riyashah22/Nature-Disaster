87425b91e00ee93130f14d37d09809662f36eab2
Vansh Shah <shah.vansh132@gmail.com>
2024-08-10 23:33:38 +0530

    support screen: All features added and completed module

--- resqaid/lib/community/community.dart
+++ resqaid/lib/community/community.dart
@@ -21,6 +21,18 @@ Future<void> _makePhoneCall(String phoneNumber) async {
 class _CommunityState extends State<Community> {
   List<Post> posts = [
     Post(
+      title: "Wayanad Floods 2024",
+      donationAmount: 35450.00,
+      deadline: "15-08-2024",
+      imageUrl:
+          "https://imgs.search.brave.com/IEoar6GORQdDedIqV9Io0s4Qc825FZQBkbPq_9keR6c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/YWxqYXplZXJhLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/My8wOC9BUDIzMjI2/MzI5NTg1NTU3LTE2/OTIwMDUzMzAuanBn/P3c9NzcwJnJlc2l6/ZT03NzAsNDk1",
+      description:
+          "The Wayanad district in Kerala was severely affected by floods and landslides in August 2024 due to intense monsoon rains. The disaster led to widespread damage to homes, agriculture, and infrastructure. Donations are needed to provide immediate relief, including shelter, food, medical assistance, and support for rebuilding efforts.",
+      postedBy: "State Disaster Management Authority",
+      location: "Wayanad, Kerala",
+      totalAmount: 67000,
+    ),
+    Post(
       title: "Himachal Pradesh Landslides 2023",
       donationAmount: 35970.00,
       deadline: "30-09-2023",
@@ -33,16 +45,28 @@ class _CommunityState extends State<Community> {
       totalAmount: 70000,
     ),
     Post(
-      title: "Wayanad Floods 2024",
-      donationAmount: 35450.00,
-      deadline: "15-08-2024",
+      title: "Assam Floods Emergency Relief",
+      donationAmount: 23750.00,
+      deadline: "30-09-2022",
       imageUrl:
-          "https://imgs.search.brave.com/IEoar6GORQdDedIqV9Io0s4Qc825FZQBkbPq_9keR6c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/YWxqYXplZXJhLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/My8wOC9BUDIzMjI2/MzI5NTg1NTU3LTE2/OTIwMDUzMzAuanBn/P3c9NzcwJnJlc2l6/ZT03NzAsNDk1",
+          "https://ichef.bbci.co.uk/news/1536/cpsprodpb/c09e/live/50608890-4e3d-11ef-93dc-c92fccfe6baf.jpg.webp",
       description:
-          "The Wayanad district in Kerala was severely affected by floods and landslides in August 2024 due to intense monsoon rains. The disaster led to widespread damage to homes, agriculture, and infrastructure. Donations are needed to provide immediate relief, including shelter, food, medical assistance, and support for rebuilding efforts.",
-      postedBy: "State Disaster Management Authority",
-      location: "Wayanad, Kerala",
-      totalAmount: 67000,
+          "The 2022 monsoon season brought severe flooding to Assam, displacing millions of people and causing significant damage to property. Donations are being collected to support rescue operations, provide medical aid, and assist in rehabilitation.",
+      postedBy: "Assam State Government",
+      location: "Assam, India",
+      totalAmount: 75000,
+    ),
+    Post(
+      title: "Maharashtra Floods 2021",
+      donationAmount: 120000.00,
+      deadline: "30-12-2021",
+      imageUrl:
+          "https://ichef.bbci.co.uk/news/1536/cpsprodpb/c09e/live/50608890-4e3d-11ef-93dc-c92fccfe6baf.jpg.webp",
+      description:
+          "The floods in Maharashtra in July 2021, caused by heavy rainfall, led to significant loss of life and property. The relief fund focuses on providing immediate assistance, including shelter, food, and medical care to affected communities.",
+      postedBy: "Maharashtra State Government",
+      location: "Maharashtra, India",
+      totalAmount: 100000,
     ),
   ];
 
@@ -213,12 +237,15 @@ class _CommunityState extends State<Community> {
                                   barrierDismissible: false,
                                   builder: (BuildContext context) {
                                     return Dialog(
+                                      backgroundColor: appColors.white,
                                       child: Padding(
-                                        padding: const EdgeInsets.all(20.0),
+                                        padding: EdgeInsets.all(20.0),
                                         child: Row(
                                           mainAxisSize: MainAxisSize.min,
                                           children: [
-                                            CircularProgressIndicator(),
+                                            CircularProgressIndicator(
+                                              color: appColors.navyBlue,
+                                            ),
                                             SizedBox(width: 20),
                                             Text(
                                                 "Adding you as a volunteer..."),
@@ -230,7 +257,7 @@ class _CommunityState extends State<Community> {
                                 );
 
                                 // Simulate a delay for the loading process
-                                Future.delayed(Duration(seconds: 2), () {
+                                Future.delayed(const Duration(seconds: 2), () {
                                   setState(() {
                                     posts[index].volunteerStatus = true;
                                   });
@@ -243,16 +270,17 @@ class _CommunityState extends State<Community> {
                                     context: context,
                                     builder: (BuildContext context) {
                                       return AlertDialog(
-                                        title: Text("Success"),
-                                        content: Text(
+                                        backgroundColor: appColors.white,
+                                        title: const Text("Success"),
+                                        content: const Text(
                                             "You have been added as a volunteer."),
                                         actions: [
-                                          TextButton(
+                                          ElevatedButton(
                                             onPressed: () {
                                               Navigator.of(context)
                                                   .pop(); // Close the confirmation dialog
                                             },
-                                            child: Text("OK"),
+                                            child: const Text("OK"),
                                           ),
                                         ],
                                       );
@@ -260,8 +288,10 @@ class _CommunityState extends State<Community> {
                                   );
                                 });
                               },
-                              label: const Text(
-                                "Become Volunteer",
+                              label: Text(
+                                posts[index].volunteerStatus
+                                    ? "Volunteered"
+                                    : "Become Volunteer",
                                 style: TextStyle(
                                   fontSize: 12,
                                 ),
@@ -271,69 +301,81 @@ class _CommunityState extends State<Community> {
                               icon: const Icon(Icons.monetization_on),
                               onPressed: () {
                                 // // Show the loading dialog with the QR code
-                                // showDialog(
-                                //   context: context,
-                                //   barrierDismissible: false,
-                                //   builder: (BuildContext context) {
-                                //     return Dialog(
-                                //       child: Padding(
-                                //         padding: const EdgeInsets.all(20.0),
-                                //         child: Column(
-                                //           mainAxisSize: MainAxisSize.min,
-                                //           children: [
-                                //             Text(
-                                //               "Scan the QR Code to Donate",
-                                //               style: TextStyle(
-                                //                   fontSize: 16,
-                                //                   fontWeight: FontWeight.bold),
-                                //             ),
-                                //             SizedBox(height: 20),
-                                //             Image.network(
-                                //               'https://example.com/your-qr-code.png', // Replace with your QR code image URL
-                                //               width: 150,
-                                //               height: 150,
-                                //             ),
-                                //             SizedBox(height: 20),
-                                //             ElevatedButton(
-                                //               onPressed: () {
-                                //                 Navigator.of(context)
-                                //                     .pop(); // Close the QR code dialog
+                                showDialog(
+                                  context: context,
+                                  barrierDismissible: false,
+                                  builder: (BuildContext context) {
+                                    return Dialog(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(20.0),
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            const Text(
+                                              "Scan the QR Code to Donate",
+                                              style: TextStyle(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            const SizedBox(height: 20),
+                                            Image.network(
+                                              'https://res.cloudinary.com/dq1q5mtdo/image/upload/v1723310293/codefury/tyv2xikrlxd6bkoqj92h.png',
+                                              width: 150,
+                                              height: 150,
+                                            ),
+                                            const SizedBox(height: 20),
+                                            ElevatedButton(
+                                              onPressed: () {
+                                                Navigator.of(context)
+                                                    .pop(); // Close the QR code dialog
 
-                                //                 // Show the payment completed dialog
-                                //                 showDialog(
-                                //                   context: context,
-                                //                   builder:
-                                //                       (BuildContext context) {
-                                //                     return AlertDialog(
-                                //                       title: Text(
-                                //                           "Payment Completed"),
-                                //                       content: Text(
-                                //                           "Thank you for your donation!"),
-                                //                       actions: [
-                                //                         TextButton(
-                                //                           onPressed: () {
-                                //                             Navigator.of(
-                                //                                     context)
-                                //                                 .pop(); // Close the payment completed dialog
-                                //                           },
-                                //                           child: Text("OK"),
-                                //                         ),
-                                //                       ],
-                                //                     );
-                                //                   },
-                                //                 );
-                                //               },
-                                //               child: Text("OK"),
-                                //             ),
-                                //           ],
-                                //         ),
-                                //       ),
-                                //     );
-                                //   },
-                                // );
+                                                setState(() {
+                                                  posts[index].donationStatus =
+                                                      true;
+                                                });
+
+                                                // Show the payment completed dialog
+                                                showDialog(
+                                                  context: context,
+                                                  builder:
+                                                      (BuildContext context) {
+                                                    return AlertDialog(
+                                                      backgroundColor:
+                                                          appColors.white,
+                                                      title: const Text(
+                                                        "Payment Completed",
+                                                      ),
+                                                      content: const Text(
+                                                        "Thank you for your donation!",
+                                                      ),
+                                                      actions: [
+                                                        ElevatedButton(
+                                                          onPressed: () {
+                                                            Navigator.of(
+                                                                    context)
+                                                                .pop();
+                                                          },
+                                                          child:
+                                                              const Text("OK"),
+                                                        ),
+                                                      ],
+                                                    );
+                                                  },
+                                                );
+                                              },
+                                              child: const Text("OK"),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                );
                               },
-                              label: const Text(
-                                "Donate",
+                              label: Text(
+                                posts[index].donationStatus
+                                    ? "Donated"
+                                    : "Donate",
                                 style: TextStyle(
                                   fontSize: 12,
                                 ),
